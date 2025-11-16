8b1b2bbcb94d78e9a1174287a50245379a856aa7
Leakbox258 <3033680748@qq.com>
2025-11-16 23:26:07 +0800

    Fix: gitignore

---
+++ include/utils/source.hpp
@@ -0,0 +1,15 @@
+#ifndef UTILS_SOURCE
+#define UTILS_SOURCE
+
+/// TODO: SourceMgr, Dump
+
+#include <cstddef>
+namespace utils {
+
+struct Location {
+  std::size_t line, col;
+};
+
+} // namespace utils
+
+#endif
